66e9e0d8026cf9d03721784f84eb3c9c5636380d
jens <jens@lea.io>
2014-12-04 11:22:19 +0100

    removing slides from starterkit

--- slides/js/watman.js
+++
@@ -1,6 +0,0 @@
-console.warn("WTF?");
-var data = new Array(10).join("wat" - 1);
-data += [32, 66, 97, 116, 109, 97, 110, 33].map(function(e){
-  return String.fromCharCode(e);
-}).join("");
-console.log({ "Ergebnis": data });
