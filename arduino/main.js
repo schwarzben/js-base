6e7703990af241569790f47d89f1a6c0059b6429
Ben Schwarz <schwarzfunk@gmail.com>
2015-01-31 13:13:04 +0100

    Simplify pin event handling, implement forwarding (WIP)
    Untested; connected socket (browser) should now be notified of pin
    value changes and supplied with pin instance and value.

--- arduino/main.js
+++ arduino/main.js
@@ -3,28 +3,19 @@ module.exports = function (socket, board, Arduino) {
     // This module will be run after the board has been conncted
     var i, pin_cap;
 
-    function handleDigitalData(pin) {
-        
-    }
-
-    function handleAnalogData(pin) {
-        
-    }
-
     function onData() {
         // Display data (may be analog or digital)
         // DEBUG
-        console.log('received data, you may inspect now');
-        console.log(arguments);
-        console.log(this); // expected to be the pin instance
-        switch (this.type) {
-            case 'analog':
-                handleAnalogData(this);
-                break;
-            case 'digital':
-                handleDigitalData(this);
-                break;
-        }
+        // console.log('received data, you may inspect now');
+        // console.log(arguments);
+        // console.log(this); // expected to be the pin instance
+        // Actually read pin data and broadcast to socket
+        // TODO maybe passing the whole pin object around is overkill
+        this.read((function (pin) {
+            return function (value) {
+                socket.broadcast('mydat', {'pin': pin, 'value': value});
+            };
+        }(this)));
     }
 
     for (i = 0; i < board.pins.length; i++) {
@@ -32,6 +23,7 @@ module.exports = function (socket, board, Arduino) {
         // check if pin_cap suitable
         if (pin_cap.supportedModes == false // falsy on empty array, e.g. on pin_cap 0
                 || pin_cap.supportedModes.indexOf(Arduino.Pin.INPUT) === -1) {
+            // pin does not support reading of values so skip it altogether
             continue;
         }
         // set pin mode to input
